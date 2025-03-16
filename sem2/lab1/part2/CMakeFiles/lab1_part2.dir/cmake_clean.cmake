64b1e7cabb50a77613ddc8fe4ce1a487afb11772
Ceilist <michailzotov@gmail.com>
2025-03-16 23:33:49 +0300

    added 1st lab

---
+++ sem2/lab1/part2/CMakeFiles/lab1_part2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_part2.dir/challengind.cpp.o"
+  "CMakeFiles/lab1_part2.dir/challengind.cpp.o.d"
+  "lab1_part2"
+  "lab1_part2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1_part2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
