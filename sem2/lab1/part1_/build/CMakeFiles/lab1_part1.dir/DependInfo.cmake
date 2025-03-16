bf90020d7dcb1a90b69d389810fcab8a7721263f
Ceilist <michailzotov@gmail.com>
2025-03-16 23:50:49 +0300

    fighting with git

---
+++ sem2/lab1/part1_/build/CMakeFiles/lab1_part1.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/mipt/cpp3sem/sem2/lab1/part1/ASCIIPicture.cpp" "CMakeFiles/lab1_part1.dir/ASCIIPicture.cpp.o" "gcc" "CMakeFiles/lab1_part1.dir/ASCIIPicture.cpp.o.d"
+  "/mnt/d/mipt/cpp3sem/sem2/lab1/part1/Cloud.cpp" "CMakeFiles/lab1_part1.dir/Cloud.cpp.o" "gcc" "CMakeFiles/lab1_part1.dir/Cloud.cpp.o.d"
+  "/mnt/d/mipt/cpp3sem/sem2/lab1/part1/Cow.cpp" "CMakeFiles/lab1_part1.dir/Cow.cpp.o" "gcc" "CMakeFiles/lab1_part1.dir/Cow.cpp.o.d"
+  "/mnt/d/mipt/cpp3sem/sem2/lab1/part1/Field.cpp" "CMakeFiles/lab1_part1.dir/Field.cpp.o" "gcc" "CMakeFiles/lab1_part1.dir/Field.cpp.o.d"
+  "/mnt/d/mipt/cpp3sem/sem2/lab1/part1/Parser.cpp" "CMakeFiles/lab1_part1.dir/Parser.cpp.o" "gcc" "CMakeFiles/lab1_part1.dir/Parser.cpp.o.d"
+  "/mnt/d/mipt/cpp3sem/sem2/lab1/part1/main.cpp" "CMakeFiles/lab1_part1.dir/main.cpp.o" "gcc" "CMakeFiles/lab1_part1.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sem2/lab1/part1_/build/CMakeFiles/lab1_part1.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_part1.dir/ASCIIPicture.cpp.o"
+  "CMakeFiles/lab1_part1.dir/ASCIIPicture.cpp.o.d"
+  "CMakeFiles/lab1_part1.dir/Cloud.cpp.o"
+  "CMakeFiles/lab1_part1.dir/Cloud.cpp.o.d"
+  "CMakeFiles/lab1_part1.dir/Cow.cpp.o"
+  "CMakeFiles/lab1_part1.dir/Cow.cpp.o.d"
+  "CMakeFiles/lab1_part1.dir/Field.cpp.o"
+  "CMakeFiles/lab1_part1.dir/Field.cpp.o.d"
+  "CMakeFiles/lab1_part1.dir/Parser.cpp.o"
+  "CMakeFiles/lab1_part1.dir/Parser.cpp.o.d"
+  "CMakeFiles/lab1_part1.dir/main.cpp.o"
+  "CMakeFiles/lab1_part1.dir/main.cpp.o.d"
+  "lab1_part1"
+  "lab1_part1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1_part1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
